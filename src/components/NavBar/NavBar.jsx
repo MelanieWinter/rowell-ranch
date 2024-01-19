e61570f2e88f2408a0f6f8b944333cddfc729303
Melanie Winter <melaniebucher830@gmail.com>
2024-01-19 13:29:23 -0800

    init commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+import * as userService from '../../utilities/users-service'
+import './NavBar.css'
+
+export default function NavBar({ user, setUser }) {
+    function handleLogOut() {
+        userService.logOut()
+        setUser()
+    }
+
+    return (
+        <nav className='NavBar'>
+            <span>Welcome, {user.name}!</span>
+            <Link to="/orders">Order History</Link>
+            {/* &nbsp; | &nbsp; */}
+            <Link to="/orders/new">New Order</Link>
+            <Link to="#" onClick={handleLogOut}>Log Out</Link>
+        </nav>
+    )
+}
